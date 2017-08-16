511906ab3fd3dff718ea9c2b527172f43be320a6
Euihoon Hwang <sudden74ko@gmail.com>
2017-08-16 17:13:45 +0900

    t1305 class 생성

--- xing.py
+++ xing.py
@@ -42,10 +42,22 @@ if __name__ == '__main__':
 
     for index, row in stock.iterrows():
 
+        # 1안
+        #1. 각 종목별로 XAQueryEvents 객체를 생성하고 eBest 데이터 연동(request 메쏘드 호출)
+        #2. 종목(stocks) 리스트에 생성된 XAQueryEvents 객체를 append
+        #3. stocks 리스트의 객체를 대상으로 loop, DataFrame을 생성
+
+        # 2안
+        #1. 각 종목별로 XAQueryEvents 객체를 생성
+        #2. 종목(stocks) 리스트에 생성된 XAQueryEvents 객체를 append
+        #3. stocks 리스트의 객체를 대상으로 loop, eBest 데이터를 연동하고(request 메쏘드 호출) DataFrame을 생성
+
+
         #instXAQuery = win32com.client.DispatchWithEvents("XA_DataSet.XAQuery", XAQueryEvents)
-        price = xingT1305.getData(row.ix[0])
+        #stocks = [XAQueryEvents(row.ix[0])]
+        #price = xingT1305.getData(row.ix[0])
         #price = xingT1305.getData(row.ix[0], instXAQuery)
-        print(price)
+        #print(price)
         #print(row.ix[0] + " reference count (after call): " + str(sys.getrefcount(XAQueryEvents)))
 
         time.sleep(1)
